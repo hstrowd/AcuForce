623dd1cb4b87949984cdbc306aefe5ff25204d7d
Ben Feigin <bbfeigin@gmail.com>
2011-07-07 12:08:18 -0500

    starting to build my conversion strategy, from omni to acunote first

--- omni_to_acunote.rb
+++ omni_to_acunote.rb
@@ -1,2 +1,22 @@
 module OmniToAcunote
+  include 'rubygems'
+  include 'nokogiri'
+
+  
+  
+  def omni_page(file_location = '/Users/bfeigin/Documents/Enova/Team')
+    @page ||= Nokogiri::HTML(open(file_location))
+  end
+
+  def omni_headers
+    @headers ||= omni_page.css(css_mapping[:headers]).children.map(&:content)
+  end
+
+
+  def css_mapping
+    @css_mapping ||= 
+      {
+        :headers => 'td.header',
+      }
+  end
 end
